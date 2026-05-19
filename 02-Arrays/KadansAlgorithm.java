0005ea33332880dc65b08bf506c84487485f21da
Suyash Lawand <suyash.lawand@gmail.com>
2026-05-19 22:04:26 +0530

    Add: Implement Kadane's Algorithm for maximum subarray sum

---
+++ 02-Arrays/KadansAlgorithm.java
@@ -0,0 +1,13 @@
+public class KadansAlgorithm {
+    public int maxSubArray(int[] nums) {
+        int sum = 0;
+        int maximum = Integer.MIN_VALUE;
+        
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (sum > maximum) maximum = sum;
+            if (sum < 0) sum = 0;    
+        }
+        return maximum;
+    }
+}
