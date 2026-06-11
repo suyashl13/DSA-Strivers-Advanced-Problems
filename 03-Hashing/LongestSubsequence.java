c377baf1f29d9390d92c96249ec7a1f437d4e693
Suyash Lawand <suyash.lawand@gmail.com>
2026-06-11 11:50:27 +0530

    Add: Implement Longest Consecutive Subsequence algorithm to find the length of the longest consecutive elements sequence

---
+++ 03-Hashing/LongestSubsequence.java
@@ -0,0 +1,31 @@
+import java.util.HashSet;
+
+public class LongestSubsequence {
+
+    public int longestConsecutive(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        int max = Integer.MIN_VALUE;
+
+        for (Integer integer : nums) 
+            set.add(integer);
+
+
+        for (int element : nums) {
+            if (set.contains(element - 1)) {
+                continue;                
+            }
+
+            int seq = 1;
+            int temp = element + 1;
+
+            while (set.contains(temp)) {
+                seq += 1;                    
+                temp += 1;
+            }
+            
+            max = Math.max(max, seq);
+        }
+        
+        return max == Integer.MIN_VALUE ? 0 : max;
+    }
+}
